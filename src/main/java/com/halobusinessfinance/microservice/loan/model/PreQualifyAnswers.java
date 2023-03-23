8c767ff7ce40ebf4e48c306a4aa2a140f18b67d7
root <root@michaels-mbp.attlocal.net>
2023-03-23 00:06:21 -0700

    HAS-20 the ability to save loans.

--- src/main/java/com/halobusinessfinance/microservice/loan/model/PreQualifyAnswers.java
+++ src/main/java/com/halobusinessfinance/microservice/loan/model/PreQualifyAnswers.java
@@ -9,6 +9,7 @@ import lombok.Data;
 
 @Data
 public class PreQualifyAnswers {
+    //common + unique 102 Bridge Loan
     private String fundPlan;
     private String refinance;
     private String cashOut;
@@ -31,7 +32,42 @@ public class PreQualifyAnswers {
     private String currentProperty;
     private String stabilized;
     private String landBuildingPurchasePrice;
-    
-   
-    
+    //unique 101 Refinance of Property
+    private String goal;
+    private String cash;
+    private String business;
+    private String address;
+    private String property;
+    private String downpayment;
+    private String commercial;
+    //unique 103 Purchase of Property
+    private String propertyState;
+    //unique 104 Franchise
+    private String businessYear;
+    private String annualRevenue;
+    private String AreaOfBusiness;
+    private String useOfProceeds;
+    private String specifiedOtherUse;
+    private String SBALoan;
+    private String creditScore;
+    private String LoanAmountRequested;
+    private String LoanTermRequested;
+    // unique 105 Factoring
+    private String invoiceReceivables;
+    private String payableTerms;
+    private String franchiseCompany;
+    //unique 106 Working Capital
+    private String grossSales;
+    private String otheruse;
+    private String franchise;
+    private String loanAmount;
+    private String ownershipPercentage;
+    //unique 107 EIDL
+    private String disasterLoan;
+    private String eIDLoanStatus;
+    private String email;
+    private String federalTaxReturn;
+    private String LoanAmountReceived;
+    private String appliedIncreaseLoan;
+    private String increaseLoanAmountReceived;
 }
